eaf6d00f00becb12de2bc7b9217ac06b52fbd487
EmJoHig <emanuel.higuera9@gmail.com>
2024-05-04 19:41:04 -0300

    ahora si que si

--- src/index.js
+++ src/index.js
@@ -11,7 +11,7 @@ import indexRoutes from "./routes/index.js";
 const app = express();
 const __dirname = dirname(fileURLToPath(import.meta.url));
 
-// settings
+// settings 
 app.set("port", process.env.PORT || 3000);
 app.set("views", join(__dirname, "views"));
 app.set("view engine", "ejs");
